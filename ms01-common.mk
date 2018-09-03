318ec66a4d110f01a16b23de354cf0a11a3c1802
Nemirtingas <9432037+Nemirtingas@users.noreply.github.com>
2018-09-03 09:31:23 +0200

    Update ms01-common.mk

--- ms01-common.mk
+++ ms01-common.mk
@@ -23,10 +23,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
     $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml
-    
-#GPS
-PRODUCT_COPY_FILES += \
-   $(LOCAL_PATH)/gps/gps.conf:system/etc/gps.conf
 
 # Media
 PRODUCT_COPY_FILES += \
